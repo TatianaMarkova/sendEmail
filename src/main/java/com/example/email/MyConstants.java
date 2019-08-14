e8d950fced32090d0517c90469f4185da9b8618a
smilyk <42746645+smilyk@users.noreply.github.com>
2019-08-14 10:49:38 +0300

    Update MyConstants.java

--- src/main/java/com/example/email/MyConstants.java
+++ src/main/java/com/example/email/MyConstants.java
@@ -2,13 +2,13 @@ package com.example.email;
 
 public class MyConstants {
     // Replace with your email here:
-    public static final String MY_EMAIL = "maEmail";
+    public static final String MY_EMAIL = "emailFrom";
 
     // Replace password!!
-    public static final String MY_PASSWORD = "maPassword";
+    public static final String MY_PASSWORD = "Password";
 
     // And receiver!
-    public static final String FRIEND_EMAIL = "smilyk1982@gmail.com";
+    public static final String FRIEND_EMAIL = "emailTo";
 
     public static final String SEND = "/sendMail";
 }
