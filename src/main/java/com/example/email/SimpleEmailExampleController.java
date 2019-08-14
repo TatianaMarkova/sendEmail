de8097263cc37e968d7656db5526b3281da5f34d
Natalya <smilyk1982@gmail.com>
2019-08-14 10:47:41 +0300

    emailWithAttachment

--- src/main/java/com/example/email/SimpleEmailExampleController.java
+++ src/main/java/com/example/email/SimpleEmailExampleController.java
@@ -1,9 +1,18 @@
 package com.example.email;
 
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
 
 
 @RestController
@@ -13,20 +22,36 @@ public class SimpleEmailExampleController {
      JavaMailSender emailSender;
 
 
-    @GetMapping(value = MyConstants.SEND)
-     String sendSimpleEmail() {
-        // Create a Simple MailMessage.
-        SimpleMailMessage message = new SimpleMailMessage();
+    @GetMapping(value = MyConstants.SEND_WITH_ATTACHMENT)
+     String sendSimpleEmail() throws MessagingException {
 
-        message.setTo(MyConstants.FRIEND_EMAIL);
-        message.setSubject("Test Simple Email");
-        message.setText("Hello, Im testing Simple Email from Java");
+        MimeMessage message = emailSender.createMimeMessage();
 
-        // Send Message!
-        this.emailSender.send(message);
+        boolean multipart = true;
+
+        MimeMessageHelper helper = new MimeMessageHelper(message, multipart);
+
+        helper.setTo(MyConstants.FRIEND_EMAIL);
+        helper.setSubject("Test email with attachments");
+
+        helper.setText("Hello, Im testing email with attachments!");
+
+        String path1 = "/Users/smilyk/Desktop/a/list.txt";
+        String path2 = "/Users/smilyk/Desktop/a/list.txt.zip";
+
+        // Attachment 1
+        FileSystemResource file1 = new FileSystemResource(new File(path1));
+        helper.addAttachment("List_id file", file1);
+
+        // Attachment 2
+        FileSystemResource file2 = new FileSystemResource(new File(path2));
+        helper.addAttachment("Jar", file2);
+
+        emailSender.send(message);
 
         return "Email Sent!";
     }
+
 
 
 }
