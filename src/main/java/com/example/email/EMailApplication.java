09c45f0351046f590c6a55903d8bc874ef317fe8
Natalya <smilyk1982@gmail.com>
2019-08-16 08:05:05 +0300

    loddig with log4j2

--- src/main/java/com/example/email/EMailApplication.java
+++ src/main/java/com/example/email/EMailApplication.java
@@ -1,16 +1,37 @@
 package com.example.email;
 
-import org.slf4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.core.layout.PatternLayout;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.LogManager;
+
+
+import java.util.regex.Pattern;
 
 @SpringBootApplication
-public class EMailApplication {
+public class EMailApplication  implements ApplicationRunner{
 
+
+    private static final Logger logger = LogManager.getLogger(EMailApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(EMailApplication.class, args);
     }
 
-
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        logger.debug("Debugging log");
+        logger.info("Info log");
+        logger.warn("Hey, This is a warning!");
+        logger.error("Oops! We have an Error. OK");
+        logger.fatal("Damn! Fatal error. Please fix me.");
+    }
 }
